d4f95f7b134fe300383b817b51d8a51a4e013575
Nikola Zorkic <n.zorkic@msgnetconomy.net>
2019-08-25 13:40:25 +0200

    Database and Domain class changes

---
+++ src/main/java/com/msgnetconomy/shop/domain/CartEntry.java
@@ -0,0 +1,97 @@
+/*********************************************************************
+ * The Initial Developer of the content of this file is NETCONOMY.
+ * All portions of the code written by NETCONOMY are property of
+ * NETCONOMY. All Rights Reserved.
+ *
+ * NETCONOMY Software & Consulting GmbH
+ * Hilmgasse 4, A-8010 Graz (Austria)
+ * FN 204360 f, Landesgericht fuer ZRS Graz
+ * Tel: +43 (316) 815 544
+ * Fax: +43 (316) 815544-99
+ * www.netconomy.net
+ *
+ * (c) 2019 by NETCONOMY Software & Consulting GmbH
+ *********************************************************************/
+
+package com.msgnetconomy.shop.domain;
+
+import java.util.Objects;
+
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+/**
+ * @author nzorkic@netconomy.net
+ */
+@Entity
+@Table(name = "cart_entry", schema = "shopdb")
+public class CartEntry {
+
+    private String code;
+    private double quantity;
+    private Product product;
+    private Cart cart;
+
+    @Id
+    @Column(name = "code")
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    @Basic
+    @Column(name = "quantity")
+    public double getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(double quantity) {
+        this.quantity = quantity;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(code, quantity);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        CartEntry cartEntry = (CartEntry) o;
+        return Double.compare(cartEntry.quantity, quantity) == 0 &&
+            Objects.equals(code, cartEntry.code);
+    }
+
+    @ManyToOne
+    @JoinColumn(name = "product_code", referencedColumnName = "code", nullable = false)
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    @ManyToOne
+    @JoinColumn(name = "cart_code", referencedColumnName = "code", nullable = false)
+    public Cart getCart() {
+        return cart;
+    }
+
+    public void setCart(Cart cart) {
+        this.cart = cart;
+    }
+}
