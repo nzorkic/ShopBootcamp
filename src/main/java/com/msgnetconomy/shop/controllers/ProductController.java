82a15611ad6b014abba8854a58ac3cc7e73a60d2
Nikola Zorkic <n.zorkic@msgnetconomy.net>
2019-09-12 00:45:20 +0200

    Solution to task 3 of day 1

--- src/main/java/com/msgnetconomy/shop/controllers/ProductController.java
+++ src/main/java/com/msgnetconomy/shop/controllers/ProductController.java
@@ -15,7 +15,12 @@
 
 package com.msgnetconomy.shop.controllers;
 
+import com.msgnetconomy.shop.services.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -25,5 +30,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/product")
 public class ProductController {
 
-    // TODO TASK 3: Create GET request method for opening product page
+    @Autowired
+    private ProductService productService;
+
+    @GetMapping("{code}")
+    public String getProductByCode(@PathVariable int code, Model model) {
+        model.addAttribute("product", productService.getProductByCode(code));
+        return "product";
+    }
 }
