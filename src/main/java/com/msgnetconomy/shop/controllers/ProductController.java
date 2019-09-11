2d8e32f3aa2fc9a8b0627ad68a905e6a0b15c97f
Nikola Zorkic <n.zorkic@msgnetconomy.net>
2019-09-12 00:24:38 +0200

    Removed imports

--- src/main/java/com/msgnetconomy/shop/controllers/ProductController.java
+++ src/main/java/com/msgnetconomy/shop/controllers/ProductController.java
@@ -15,15 +15,8 @@
 
 package com.msgnetconomy.shop.controllers;
 
-import com.msgnetconomy.shop.services.ProductService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import static com.msgnetconomy.shop.controllers.constants.ControllerConstants.Pages.PRODUCT;
 
 /**
  * @author nzorkic@netconomy.net
--- src/main/java/com/msgnetconomy/shop/controllers/RegistrationController.java
+++ src/main/java/com/msgnetconomy/shop/controllers/RegistrationController.java
@@ -17,18 +17,15 @@ package com.msgnetconomy.shop.controllers;
 
 import com.msgnetconomy.shop.domain.User;
 import com.msgnetconomy.shop.services.CartService;
-import com.msgnetconomy.shop.services.UserService;
 import com.msgnetconomy.shop.utils.ErrorUtils;
 import com.msgnetconomy.shop.validators.UserValidator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import static com.msgnetconomy.shop.controllers.constants.ControllerConstants.Pages.LOGIN;
